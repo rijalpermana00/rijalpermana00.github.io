882738483fee1d2b502c8eb957eeccfc74d13e67
rijal <rijal@neuronworks.co.id>
2020-07-05 23:49:19 +0700

    mencoba hello world

--- resources/views/layout/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="container my-auto">
-  <div class="copyright text-center my-auto">
-    <span>Copyright &copy; Blacktorrent 2020</span>
-  </div>
-</div>
--- resources/views/layout/script.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- Bootstrap core JavaScript-->
-<script src="assets/jquery/js/jquery.min.js"></script>
-<script src="assets/jqueryui/js/jqueryui.min.js"></script>
-<script src="assets/bootstrap/js/bootstrap.min.js"></script>
-<script src="assets/ckeditor/ckeditor.js"></script>
-<!-- Custom scripts for all pages-->
-<script src="assets/adminsb/js/sb-admin-2.min.js"></script>
